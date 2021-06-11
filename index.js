460e158735ea17d937f1be9367f58b3f29ec8bda
Anna Wilson <ajoyna01@gmail.com>
2021-06-11 15:53:52 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+//const found = array1.find(element => element > 10);
 
+  
+  const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+
+  ] 
+  function superbowlWin(record) {
+    
+    const winner = record.find((r) => r.result==="W") 
+    return winner ? winner.year : undefined
+  }
+
+//TC helped me use a ternary statement to pass both tests. Still need practice on callback fn
